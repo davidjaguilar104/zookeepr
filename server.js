8681b83a72764130891903641d40fa75454ee997
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-11-23 12:18:08 -0600

    integrated front end/added more middleware

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// provide file path to location in our app (the 'public' folder) and instruct server to make the files inside this directory static resources 
+app.use(express.static('public'));
+
 // parse incoming string or array data 
 app.use(express.urlencoded({extended: true}));
+
 // parse incoming JSON data
 app.use(express.json());
 
@@ -57,7 +61,7 @@ function findById(id, animalsArray) {
 };
 
 function createNewAnimal(body, animalsArray) {
-    console.log(body); // prints req.body passed in below in POST
+    // console.log(body); // prints req.body passed in below in POST
 
     // our function's main code will go here!
     const animal = body;
@@ -122,6 +126,22 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
